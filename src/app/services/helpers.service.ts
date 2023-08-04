c20139b1452154ddf1866a90703e8eb3778879a2
Martin Grellet <martin.alejandro.grellet@gmail.com>
2023-08-03 22:54:28 -0300

    share functionality in create pdf

--- src/app/services/helpers.service.ts
+++ src/app/services/helpers.service.ts
@@ -1,36 +1,40 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import * as pdfMake from "pdfmake/build/pdfmake";
 import {Rent} from "../interfaces/rent";
+import {Platform} from "@ionic/angular";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HelpersService {
 
-  constructor() { }
+  constructor(private platform: Platform) {
+  }
 
   generatePdf(rent: Rent) {
     console.log("rent", rent);
+    console.log(this.platform.is('mobileweb'));
+
     // Sample invoice data
     const invoice = {
       number: 'INV-001',
       date: '2023-08-03',
       recipient: 'John Doe',
       items: [
-        { description: 'Product A', quantity: 2, price: 50 },
-        { description: 'Product B', quantity: 1, price: 75 },
-        { description: 'Product C', quantity: 3, price: 30 },
+        {description: 'Product A', quantity: 2, price: 50},
+        {description: 'Product B', quantity: 1, price: 75},
+        {description: 'Product C', quantity: 3, price: 30},
       ],
       total: 255,
     };
 
     // Define the content for the PDF
     const content = [
-      { text: 'Invoice', style: 'header' },
+      {text: 'Invoice', style: 'header'},
       `Invoice Number: ${invoice.number}`,
       `Date: ${invoice.date}`,
       `Recipient: ${invoice.recipient}`,
-      { text: 'Items', style: 'subheader' },
+      {text: 'Items', style: 'subheader'},
       {
         table: {
           headerRows: 1,
@@ -41,7 +45,7 @@ export class HelpersService {
           ],
         },
       },
-      { text: `Total: $${invoice.total}`, alignment: 'right', margin: [0, 20, 0, 0] },
+      {text: `Total: $${invoice.total}`, alignment: 'right', margin: [0, 20, 0, 0]},
     ];
 
     // Define the document definition
@@ -63,6 +67,35 @@ export class HelpersService {
 
     // Generate the PDF
     // @ts-ignore
-    pdfMake.createPdf(docDefinition).download(`invoice_${invoice.number}.pdf`);
+    const pdfDocGenerator = pdfMake.createPdf(docDefinition);
+
+    // Convert the PDF object to a data URL or Blob
+    pdfDocGenerator.getBlob((pdfBlob) => {
+      // Create a Blob URL from the Blob
+      const pdfFile = new File([pdfBlob], `invoice_${invoice.number}.pdf`, {type: 'application/pdf'});
+
+      // Check if the Web Share API is supported
+      if (navigator.share) {
+        navigator
+          .share({
+            files: [pdfFile], // Pass the Blob URL to navigator.share
+            title: 'Invoice PDF',
+          })
+          .then(() => {
+            console.log('Shared successfully');
+          })
+          .catch((error) => {
+            console.error('Error sharing:', error);
+          });
+      } else {
+        // Fallback for devices/browsers that don't support Web Share API
+        console.log('Web Share API is not supported on this device.');
+        // Implement your own fallback sharing method here (if needed).
+      }
+    });
+
+    // Generate the PDF
+    // @ts-ignore
+    //pdfMake.createPdf(docDefinition).download(`invoice_${invoice.number}.pdf`);
   }
 }
