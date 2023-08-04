d555a3707b7ce7e684454eacf580335629f591c9
Martin Grellet <martin.alejandro.grellet@gmail.com>
2023-08-03 23:14:30 -0300

    firebase files

---
+++ readme.md
@@ -0,0 +1,18 @@
+Steps to deploy
+
+npm install -g firebase-tools
+
+set environment.prod.ts
+
+npm run build
+
+
+firebase login               
+
+firebase init
+  * hosting
+  * www
+  * y
+  * n
+
+firebase deploy
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCviqiXTqayBJ58BwOH-zywzTiASje_jvE",
+    authDomain: "picante-trajes.firebaseapp.com",
+    projectId: "picante-trajes",
+    storageBucket: "picante-trajes.appspot.com",
+    messagingSenderId: "729380766451",
+    appId: "1:729380766451:web:cce059885d5d32b2a3ed86"
+  }
 };
